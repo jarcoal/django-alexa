e14612e6b04b863f6bedcb7d3a6fdf096ed5c7ea
Jared Morse <jarcoal@gmail.com>
2016-10-16 17:39:34 -0700

    add package metadata to setup() call

--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ def open_file(fname):
     return open(path.join(path.dirname(__file__), fname))
 
 setup(
+    name='django-alexa',
+    author='Kyle Rockman',
+    author_email='kyle.rockman@mac.com',
+    summary='Amazon Alexa Skills Kit integration for Django',
+    url='https://github.com/rocktavious/django-alexa',
+    description='* Documentation: https://github.com/rocktavious/django-alexa',
+    license_file='LICENSE'
     install_requires=open(path.join(path.dirname(__file__), 'requirements.txt')).readlines(),
 )
